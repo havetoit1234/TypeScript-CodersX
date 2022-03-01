a5c34011cccb705652be610862d15467f3dfa2be
havetoit1234 <havetoit1234@gmail.com>
2022-03-01 12:01:12 +0700

    'uploadcode'

---
+++ InterFace_Merning/ts-demo-template/src/index.ts
@@ -0,0 +1,24 @@
+interface Contact {
+  name : string , 
+  age : number , 
+  phone : string
+}
+
+const newContact:Contact[] = []
+const contact:Contact = {
+  name : 'nguyen van a',
+  age : 12,
+  phone : '5688'
+}
+/* Merning interface */
+const otherContact = {
+  name : 'nguyen vawn C',
+  age : 12,
+  phone : '1236'
+}
+/* day la cach binh thuong */
+/* const otherContact:Contact = {
+  name : 'nguyen van c',
+  age : 12,
+  phone : '12354'
+} */
